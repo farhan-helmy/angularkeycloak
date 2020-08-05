52807cb5e5cf48235dc3712d72281c6699330b57
farhanhlmy <farhanhlmy@gmail.com>
2020-08-05 12:57:42 +0800

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularnew';
+
+  client_id: 'angular-test-app'
+  grant_type: 'password'
+  client_secret: '9ddb545f-a9e0-4ff0-a8ea-f0ceee0012d9'
+  scope: 'openid'
+  username: 'hpupm'
+  password: 'password'
+
+  result: String
+
+  //json;
+  constructor(private http: HttpClient) {
+  }
+
+  postData() {
+
+    let url = "http://10.150.236.115:8080/auth/realms/MainSSORealm/protocol/openid-connect/token";
+
+    let body = new URLSearchParams();
+    body.set('client_id',this.client_id);
+    body.set('grant_type',this.grant_type);
+    body.set('client_secret',this.client_secret);
+    body.set('scope',this.scope);
+    body.set('username',this.username);
+    body.set('password',this.password);
+
+    //let url = "http://httpbin.org/post"
+
+    this.http.post(url, body).toPromise().then((data: any) => {
+      console.log(data)
+
+      this.result = JSON.stringify(data.json)
+    })
+  }
 }
+
+//9ddb545f-a9e0-4ff0-a8ea-f0ceee0012d9
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+//import { NgModel } from '@angular/ng-model';
+//import { AppRoutingModule } from ''
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
